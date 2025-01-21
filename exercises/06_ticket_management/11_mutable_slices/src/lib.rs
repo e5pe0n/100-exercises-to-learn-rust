2c713481c3a20208b07ab1c09d14a81314adc90d
e5pe0n <1izard.synchr0fi5h@gmail.com>
2025-01-21 22:42:49 +0900

    06_ticket_management/11_mutable_slices

--- exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -1,5 +1,10 @@
 // TODO: Define a function named `squared` that raises all `i32`s within a slice to the power of 2.
 //  The slice should be modified in place.
+fn squared(ns: &mut [i32]) {
+    for n in ns.iter_mut() {
+        *n *= *n;
+    }
+}
 
 #[cfg(test)]
 mod tests {
